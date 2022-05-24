b837df3f53a60cdae27037b7de358eab83d880ad
rajshankarM <rajshankar607@gmail.com>
2022-05-24 11:22:18 +0530

    update

--- app.js
+++ app.js
@@ -3,19 +3,17 @@ import mongoose from "mongoose";
 import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 import cors from "cors";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(cors());
-// dotenv.config();
+dotenv.config();
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
 mongoose
-  .connect(
-    "mongodb+srv://rajshankar:14311135@cluster0.vd1o5.mongodb.net/Blog?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGO_URL)
   .then(() => app.listen(5000))
   .then(() => console.log("Connected to database😊"))
   .catch((err) => console.log(err));
