378e7ba17524fd69a2655ce2ebb02831cec2085e
rajshankarM <rajshankar607@gmail.com>
2022-05-24 10:09:48 +0530

    update

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 import cors from "cors";
-import dotevn from "dotenv";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(cors());
-dotevn.config();
+dotenv.config();
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
